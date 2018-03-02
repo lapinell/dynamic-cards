6f8479291ad8acbef548b93fc334a10e9710a2bb
lapinell <lapinell@gmail.com>
2018-03-01 23:09:36 -0600

    added generate id functions

--- cards.js
+++ cards.js
@@ -8,38 +8,58 @@ function card (content, cardID, deleteID) {
     this.deleteID = deleteID
 };
 
-// Generate cardIDs //
+// Store new cards in an array
 
-let generateCardId = () => {
-
-};
-
-// Generate delete IDs //
-
-let generateDeleteId = () => {
-
-};
-
-// Add user content to card//
-
-var newCard = new card('Card content says not much of anything', 'T-CID001', 'T-DID001');
-
-console.log(newCard);
-
-// Store new cards in an array //
+const storeCards = [];
 
 // Print card to DOM //
-let createCard = (userContent) => {
-    let newCard = new card (userContent, 'T-CID001', 'T-DID001');
+let createCard = (userContent, cID, dID) => {
+    console.log('creating card');
+    let newCard = new card (userContent,cID, dID);
+    storeCards.push(newCard);
+    console.log(storeCards);
     let cardContent = '';
     cardContent += `<article id="${newCard.cardID}">`;
     cardContent += `<p>${newCard.content}</p>`;
     cardContent += `<button id="${newCard.deleteID}">Delete</button>`;
     cardContent += `</article>`;
 
-    document.getElementById('contentContainer').innerHTML = cardContent;
+    document.getElementById('contentContainer').innerHTML += cardContent;
+    console.log('card created');
 }
 
-createCard('Sample text for the card.');
+// Event Listener for actual user input
+
+let createButton = document.getElementById('createContent');
+createButton.addEventListener("click", function() {
+
+    // Grab user content
+    let userInput = document.getElementById('contentInput').value;
+
+    // Generate cardIDs
+    let cID = 'CID';
+    let generateCardId = () => {
+        let newValue = 1;
+        cID += newValue;
+        return cID;
+    };
+
+    generateCardId();
+    console.log('new CID:', cID);
+
+    // Generate delete IDs
+    let dID = 'DID';
+    let generateDeleteId = () => {
+        let newValue = 1;
+        dID += newValue;
+        return dID;
+    };
+
+    generateDeleteId();
+    console.log('new DID:', dID);
+
+    //Create card
+    createCard(userInput, cID, dID);
+});
 
 // Delete text from DOM
