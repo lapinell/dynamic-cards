d692ee09617732165c5c96b11185d339b8568c5f
lapinell <lapinell@gmail.com>
2018-03-03 11:49:03 -0600

    created MutationObserver to contain delete button event listener

--- cards.js
+++ cards.js
@@ -14,7 +14,7 @@ let generateCardId = () => {
     let newCidValue = 'CID';
     cIDvalue += 1;
     newCidValue += cIDvalue.toString();
-    console.log('newValue:', newCidValue);
+    // console.log('newValue:', newCidValue);
     return newCidValue;
 };
 
@@ -25,12 +25,14 @@ let generateDeleteId = () => {
     let newDidValue = 'DID';
     dIDvalue += 1;
     newDidValue += dIDvalue.toString();
-    console.log('newValue:', newDidValue);
+    // console.log('newValue:', newDidValue);
     return newDidValue;
 };
 
+/////////////////////////////////////////////////////
 
-// Build Card
+// Build Card //
+
 function buildCard () {
 
     // Grab user content
@@ -44,9 +46,11 @@ function buildCard () {
 
 const storeCards = [];
 
+/////////////////////////////////////////////////////
+
 // Print card to DOM //
 let createCard = (userContent, cID, dID) => {
-    console.log('creating card');
+    // console.log('creating card');
     let newCard = new card (userContent,cID, dID);
     storeCards.push(newCard);
    
@@ -57,10 +61,12 @@ let createCard = (userContent, cID, dID) => {
     cardContent += `</article>`;
 
     document.getElementById('contentContainer').innerHTML += cardContent;
-    console.log('card created');
+    // console.log('card created');
 
     return storeCards;
 }
+
+// I could create a function that loads all the cards in the array of objects, then prints them all but no - that would cause the entire cardContainer to reload everytime a new card was created or deleted.
 
 console.log('stored cards:', storeCards);
 // Event Listener for actual user input
@@ -68,12 +74,63 @@ console.log('stored cards:', storeCards);
 let createButton = document.getElementById('createContent');
 createButton.addEventListener("click", function() { buildCard(); console.log(storeCards); });
 
-// Check the last cID number
+/////////////////////////////////////////////////////
 
-//// unstringify the CID and number
+// Delete text from DOM //
 
-//// find the highest number
+// Find delete button id
 
-//// return the highest number
+let cardContainer = document.getElementById('contentContainer');
+console.log(cardContainer);
 
-// Delete text from DOM
+var config = { childList: true };
+
+var callback = function(mutationsList) {
+    console.log('mutationList:', mutationsList)
+    for(var mutation of mutationsList) {
+        console.log('mutation:', mutation);
+        if (mutation.type == 'childList') {
+            console.log('A child node has been added or removed');
+            let deleteButton = document.getElementById('DID1'); //DID1 does not exist on page load, until it is created
+            deleteButton.addEventListener("click", function() {
+                console.log('deleteButton has been clicked');
+            })
+        }
+    }
+}
+
+var observer = new MutationObserver(callback);
+
+observer.observe(cardContainer, config);
+
+// cardContainer.addEventListener('DOMNodeInsertedIntoDocument', function () { 
+//     console.log('DOM changed'); 
+//     console.log('inside EL', cardContainer);
+
+//     let deleteButton = document.getElementById('DID1'); //DID1 does not exist on page load, until it is created
+//     console.log('deleteButton', deleteButton);
+// // Event listener for deletion of card
+
+// deleteButton.addEventListener("click", function () { 
+//     console.log('delete button was clicked');
+
+
+
+// // Find associated card id
+
+// let cardToDelete = document.getElementById('CID1');
+// console.log('cardToDelete:', cardToDelete);
+
+// // Delete card from array
+
+
+
+// // Delete card from DOM
+
+// let parentElement = cardToDelete.parentNode;
+//     console.log('start', deleteButton);
+//  parentElement.removeChild(cardToDelete); 
+//     console.log('end');
+// });
+
+// });
